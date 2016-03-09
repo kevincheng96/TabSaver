9aef00c8144b81b41945302f839b0efad1083de9
kevincheng96 <kevincheng96@hotmail.com>
2016-03-08 18:23:32 -0600

    added Save Current Session functionality

--- popup.js
+++ popup.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
 	checkSaveWindowButton.addEventListener('click', function() {
 		saveWindow();
 	}, false);
+	var checkSaveWindowButton = document.getElementById('savesession');
+	checkSaveWindowButton.addEventListener('click', function() {
+		saveSession();
+	}, false);
 }, false);
 
 
@@ -81,6 +85,49 @@ function saveWindow() {
 					console.log(url_short);
 					console.log('not already saved');
 				}
+			});
+			//first delete the old urls from storage, then add the new urls into storage
+			chrome.storage.local.remove('urls', function() {
+				//locally save url_dict_list (containing the new url)
+				chrome.storage.local.set({'urls': url_dict_list}, function() {
+					//callback function (maybe show that tab was successfully added)
+				});
+			});
+		});	
+	});
+};
+
+ //Saves all tabs in the current session
+function saveSession() {
+	//retrieve the old url_dict_list from local storage
+	chrome.storage.local.get('urls', function(result) {
+		console.log(result)
+		var url_dict_list = []; //used to keep track of the dictionary for each url/url_short/jpg we hvae
+		var url_list = []; //actual url from the dictionary
+		if (!($.isEmptyObject(result))) //if returned object is not empty, append it to url_dict_list
+		{
+			var length = result['urls'].length;
+			for (var i = 0; i < length; i++)
+			{
+				url_dict_list.push(result['urls'][i]);
+				url_list.push(result['urls'][i]['url']);
+				console.log(result['urls'][i]);
+			}
+		}
+		//get the URL of the current tab and save it in the url_dict_list
+		chrome.windows.getAll({populate: true}, function(windows) {
+			windows.forEach(function(w) {
+				w['tabs'].forEach(function(tab) {
+					var tab_url = tab.url;
+					var url_short = tab.title;
+					var url_dict = {'url': tab_url, 'url_short': url_short}
+					if ($.inArray(tab_url, url_list) == -1) //if current URL is not already saved
+					{
+						url_dict_list.push(url_dict);
+						console.log(url_short);
+						console.log('not already saved');
+					}
+				});
 			});
 			//first delete the old urls from storage, then add the new urls into storage
 			chrome.storage.local.remove('urls', function() {
